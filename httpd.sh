f3b285bd3b0f426747e213a335cf0e6e9642b0f8
naveen841 <79522681+naveen841@users.noreply.github.com>
2025-12-13 18:06:26 +0530

    Update httpd.sh

--- httpd.sh
+++ httpd.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo yum update -y
+yum update -y
 
-sudo yum install httpd -y
+yum install httpd -y
 
 if [[ $1 -eq 0 ]];
 then 
     echo "please check if its has installed"
 fi
 
-sudo systemctl start httpd
+systemctl start httpd
